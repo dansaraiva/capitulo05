7bf45c8841c5996b24c4021299b938cd76473779
Daniel Saraiva <saraivadaniel@protonmail.com>
2023-10-03 15:48:12 -0300

    Atividade06 - Cap05 - com char

--- Atividade06.java
+++ Atividade06.java
@@ -13,20 +13,24 @@ public class Atividade06 {
      */
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        String codigo;
+        //String codigo;
         double totalAVista = 0, totalAPrazo = 0, valorCompra, total;
-       
+        char codigo;
+      
         for (int i = 0; i < 15; i++) {
-            System.out.print("Informe código: ");
-            codigo = entrada.next().toUpperCase();
+            System.out.print("Informe código: "); 
+            codigo = entrada.next().charAt(0);
+            //codigo = entrada.next().toUpperCase();
     
             System.out.print("Informe valor compra: ");
             valorCompra = entrada.nextDouble();
 
-            if (codigo.equals("V")) {
+            if (codigo == 'V' || codigo == 'v') {
+                //codigo.equals("V")
                 totalAVista = totalAVista + valorCompra;
                 //totalAVista += valorCompra
-            } else if (codigo.equals("P")) {
+            } else if (codigo == 'P' || codigo == 'p') {
+                //codigo.equals("P")
                 totalAPrazo = totalAPrazo + valorCompra;
                 //totalAPrazo += valorCompra
             } else {
